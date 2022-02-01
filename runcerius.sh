0e1a27d5de0b296dc413513b36e5207efe301f70
Nathan DeYonker <ndyonker@memphis.edu>
2022-02-01 17:08:33 -0600

    updated path for running cerius on new leviathan machine

--- runcerius.sh
+++ runcerius.sh
@@ -12,10 +12,10 @@ if [ "$1" = "--help" ]; then
 elif [ "$1" = "-unlock" ]; then 
   . /home/accelrys/License_Pack/msi_lic_profile
   export LD_LIBRARY_PATH=/usr/X11R6/lib:/lib
-  /home/accelrys/cerius2_c410L/bin/cerius2 -unlock
+  /home/accelrys/cerius2_c411/bin/cerius2 -unlock
 else
   . /home/accelrys/License_Pack/msi_lic_profile
   export LD_LIBRARY_PATH=/usr/X11R6/lib:/lib
-  /home/accelrys/cerius2_c410L/bin/cerius2 
+  /home/accelrys/cerius2_c411/bin/cerius2 
 fi
 

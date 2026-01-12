c1040d9dce3bd8324aec69b0a6517c9fa2933ef7
Dominique Ann Wappett <dwappett@memphis.edu>
2026-01-12 11:26:44 -0600

    update job checking to handle orca errors and freq restarts

--- dwappett/check_new_jobs.sh
+++ dwappett/check_new_jobs.sh
@@ -5,19 +5,35 @@ SECONDS=0
 for i in $(cat new_dirs.txt); do
  echo $i
  cd $i
+ rm check-new-jobs_done.txt check-new-jobs_maxcyc.txt check-new-jobs_freqcrash.txt check-new-jobs_optcrash.txt check-new-jobs_orcaerror.txt check-new-jobs_CHECK_MANUALLY.txt 2> /dev/null
  for d in f*; do
-  if grep -q "ORCA TERMINATED NORMALLY" $d/orca.out; then
-   if grep -q "THE OPTIMIZATION HAS CONVERGED" $d/orca.out; then echo $d >> check-new-jobs_done.txt
-   elif grep -q "The optimization did not converge" $d/orca.out; then echo $d >> check-new-jobs_maxcyc.txt
+   if grep -q "Geometry Optimization Run" $d/orca.out; then
+     if grep -q "ORCA TERMINATED NORMALLY" $d/orca.out; then
+      if grep -q "THE OPTIMIZATION HAS CONVERGED" $d/orca.out; then echo $d >> check-new-jobs_done.txt
+      elif grep -q "The optimization did not converge" $d/orca.out; then echo $d >> check-new-jobs_maxcyc.txt
+      else echo "$d - orca.out terminated normally but neither converged nor hit max cycles" >> check-new-jobs_CHECK_MANUALLY.txt
+      fi
+     else
+      if grep -q "THE OPTIMIZATION HAS CONVERGED" $d/orca.out; then echo $d >> check-new-jobs_freqcrash.txt
+      elif grep -q "aborting the run" $d/orca.out; then echo $d >> check-new-jobs_orcaerror.txt
+      else echo $d >> check-new-jobs_optcrash.txt
+      fi
+     fi
+   elif grep -q "Energy+Gradient Calculation" $d/orca.out; then
+     optout=0
+     for j in $(ls $d/*-out 2>/dev/null); do if grep -q "Geometry Optimization Run" $j; then optout=$j; fi; done
+     if [[ "$optout" != "0" ]] && grep -q "THE OPTIMIZATION HAS CONVERGED" $optout; then
+      if grep -q "ORCA TERMINATED NORMALLY" $d/orca.out; then echo $d >> check-new-jobs_done.txt
+      else echo $d >> check-new-jobs_freqcrash.txt
+      fi
+     else echo "$d - can't find converged opt output but orca.out is only calculating frequencies" >> check-new-jobs_CHECK_MANUALLY.txt
+     fi
+   else echo "$d - orca.out does not seem to be a geometry optimization or frequency calculation" >> check-new-jobs_CHECK_MANUALLY.txt
    fi
-  else 
-   if grep -q "HURRAY" $d/orca.out; then echo $d >> check-new-jobs_freqcrash.txt
-   else echo $d >> check-new-jobs_optcrash.txt
-   fi
-  fi
  done
  cd ..
 done
+
 
 if (( $SECONDS > 3600 )) ; then
     let "hours=SECONDS/3600"

2572891d6df346227f82e4647bd7c400e7a894b1
Dominique Ann Wappett <dwappett@memphis.edu>
2026-01-15 14:04:26 -0600

    create 1-array with redo-freq.sh

--- dwappett/redo-freq.sh
+++ dwappett/redo-freq.sh
@@ -3,6 +3,7 @@
 # redo-freq.sh arg1
 # arg1 is file listing dirs to do this for
 
+joblist="none"
 for i in $(cat $1); do
  cd $i
  #orca-extract-final-geom.sh orca.out 
@@ -16,9 +17,19 @@ for i in $(cat $1); do
   if (( $(ls *freqcrash-inp 2>/dev/null) )) && cmp -s $(ls *-freqcrash-inp | tail -1) orca.inp; then
    replace_orca_inp_geom.py
    sed -i "s/opt //" orca.inp
-  else echo "error propagating orca.inp for $i, not overwriting"
+   if [[ "$joblist" == "none" ]]; then
+    joblist=$((10#${j#f}))
+   else
+    joblist=$joblist","$((10#${j#f}))
+   fi
+  else echo "error propagating orca.inp for $i, not overwriting" >> freq-restart-issues.txt
   fi
- else echo "error retrieving optimized coordinates for $i"
+ else echo "error retrieving optimized coordinates for $i" >> freq-restart-issues.txt
  fi
  cd -
 done
+
+cp ~/git/DeYonker-lab-scripts/dwappett/1-array 1-array-redo-freqcrash
+sed -i "s/ASTART-AEND\%4/${joblist}%1/" 1-array-redo-freqcrash
+
+
